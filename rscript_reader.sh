4bc9439d2702b1d76ca0db41c904471a22315224
SHUNSUKE-N <s.nishino.blue.moon@gmail.com>
2021-02-17 01:31:59 +0900

    WIP

--- rscript_reader.sh
+++ rscript_reader.sh
@@ -1,7 +1,9 @@
-bitcoind -daemon << EOF
-curl --user rpcuser --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getchaintips", "params": [] }' -H 'content-type: text/plain;' http://127.0.0.1:8332/ > curl_result.txt
-rpcpassword
-EOF
+cd /Users/shunsuke/bitcoin_notice_script/
+bitcoind -daemon
+sleep 30s
+bitcoin-cli getchaintips > latest_curl.json
+sleep 10s
+bitcoin-cli stop
 irb << EOF
 load "/Users/shunsuke/bitcoin_notice_script/ruby_script.rb"
 exit
