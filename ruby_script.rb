146e0faa05f6229445c77f0a1690106c6fff0bb0
SHUNSUKE-N <s.nishino.blue.moon@gmail.com>
2021-02-17 22:23:01 +0900

    要件に合わせて通知内容を修正

--- ruby_script.rb
+++ ruby_script.rb
@@ -11,9 +11,9 @@ def get_current_hash
   JSON.parse(res.body)
 end
 
-def slack_notice(diff:, message:)
+def slack_notice(diff:)
   uri  = URI.parse('WEBHOOK_URL')
-  params = { username: 'notice_bot', channel: 'latest_block_notice', text: "latest block numberに差分#{diff}のdiffが生まれました（from #{message}）" }
+  params = { username: 'notice_bot', channel: 'latest_block_notice', text: "latest block numberに差分#{diff}のdiffが生まれています" }
   https = Net::HTTP.new(uri.host, uri.port)
   https.use_ssl = true
   https.start do
@@ -23,22 +23,8 @@ def slack_notice(diff:, message:)
   end
 end
 
-last_curl_num = File.read('/Users/shunsuke/bitcoin_notice_script/last_curl.txt').to_i
 latest_curl_num = File.read('/Users/shunsuke/bitcoin_notice_script/latest_curl.json').split(':')[1].strip.split(',')[0].to_i
-curl_diff = latest_curl_num - last_curl_num
-
-last_api_num = File.read('/Users/shunsuke/bitcoin_notice_script/last_api.txt').to_i
 latest_api_num = get_current_hash['height'].to_i
-api_diff = latest_api_num - last_api_num
+diff = latest_curl_num - latest_api_num
 
-if api_diff > 2 || api_diff < -2
-  api_message = 'Blockchain Data API'
-  slack_notice(diff: api_diff, message: api_message)
-  File.write('/Users/shunsuke/bitcoin_notice_script/last_api.txt', latest_api_num)
-end
-
-if curl_diff > 2 || curl_diff < -2
-  bitcoind_message = 'bitcoind'
-  slack_notice(diff: curl_diff, message: bitcoind_message)
-  File.write('/Users/shunsuke/bitcoin_notice_script/last_curl.txt', latest_curl_num)
-end
+slack_notice(diff: diff)　if diff > 2 || diff < -2
